651fd1169021e790e39e605bf7127eeaf2d88053
nn98 <jkllhgb@gmail.com>
2020-02-11 20:28:33 +0900

    def PK class

---
+++ RB0/src/main/java/net/skhu/domain/DefPK.java
@@ -0,0 +1,18 @@
+package net.skhu.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.Data;
+
+@Data
+public class DefPK implements Serializable {
+	
+	@ManyToOne
+	@JoinColumn(name="id")
+	Book book;
+	
+	String contents;
+}
