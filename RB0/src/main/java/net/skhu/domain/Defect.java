4879dbc8fc829a4fb675916b163f6eba990b39dd
nn98 <jkllhgb@gmail.com>
2020-02-11 20:04:30 +0900

    defect cre

---
+++ RB0/src/main/java/net/skhu/domain/Defect.java
@@ -0,0 +1,18 @@
+package net.skhu.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class Defect {
+	
+	@ManyToOne
+	@JoinColumn(name="id")
+	Book book;
+	
+	String contents;
+}
