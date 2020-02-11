8578964f055fb9194171dc9dc50472f7eec49c03
nn98 <jkllhgb@gmail.com>
2020-02-11 20:29:28 +0900

    userRepo mod: PW censored

--- RB0/src/main/java/net/skhu/repository/UserRepository.java
+++ RB0/src/main/java/net/skhu/repository/UserRepository.java
@@ -1,9 +1,17 @@
 package net.skhu.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import net.skhu.domain.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+	
+	public default List<User> findAllWithoutPW(){
+		List<User> l=this.findAll();
+		for(User u:l)u.setPassword("UNSATISFIED");
+		return l;
+	}
 
 }
