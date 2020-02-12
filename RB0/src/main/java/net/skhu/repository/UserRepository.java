c4573fb8cd6009a33c017caa30a25f122e8d9d2a
nn98 <jkllhgb@gmail.com>
2020-02-13 04:31:20 +0900

    error check

--- RB0/src/main/java/net/skhu/repository/UserRepository.java
+++ RB0/src/main/java/net/skhu/repository/UserRepository.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import net.skhu.domain.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
@@ -13,5 +15,27 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 		for(User u:l)u.setPassword("UNSATISFIED");
 		return l;
 	}
-
+	
+//	@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+//	@JsonIgnore
+//	public User getOne(Integer id);
+	
+//	@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+//	@JsonIgnore
+//	public List<User>findAll();
+	
+//	@Query("insert into user(id,name,password,departmentid,grade,agree) values(%?1%,%?2%,%?3%,%?4%,%?5%,%?6%)")
+	
+//	@Query("insert into user(id,name,password,departmentid,grade,agree) "
+//			+ "values(#{user.id},#{user.name},#{user.password},#{user.department.id},#{user.grade},#{user.agree}")
+	
+//	@Query("insert into user(id,name,password,departmentid,grade,agree) "
+//			+ "values(${user.id},${user.name},${user.password},${user.department.id},${user.grade},${user.agree}")
+//	public void saveWithHashing(@Param("user") User user);
+	
+//	@Query("insert into user(id,name,password,departmentid,grade,agree) "
+//			+ "values(#{user.id},#{user.name},#{user.password},#{user.department.id},#{user.grade},#{user.agree}")
+//	void saveWithHashing(@Param("user") User user);
+	
+	
 }
