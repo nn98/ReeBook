0f9a2905c262b7ea2572e24c4b95a835dce97d60
nn98 <jkllhgb@gmail.com>
2020-03-14 20:34:51 +0900

    #7 up #8 set

--- RB0/src/main/java/net/skhu/repository/RentRepository.java
+++ RB0/src/main/java/net/skhu/repository/RentRepository.java
@@ -12,8 +12,15 @@ public interface RentRepository extends JpaRepository<Rent, Integer> {
 	default List<Integer> findAllBookId(){
 		List<Rent> rlist=this.findAll();
 		List<Integer> blist=new ArrayList();
-		for(Rent r:rlist) blist.add(r.getBid());
+		for(Rent r:rlist) blist.add(r.getBook().getId());
 		return blist;
 	}
 
+	default List<Rent> findByUserId(int id){
+		List<Rent> re=new ArrayList();
+		for(Rent r:this.findAll())
+			if(r.getUser().getId()==id)
+				re.add(r);
+		return re;
+	}
 }
